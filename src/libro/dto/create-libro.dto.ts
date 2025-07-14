a1615925265fb0afa93a12b9d4638e1b62988aa8
Ian Joel <ijalvarez@espe.edu.ec>
2025-07-14 13:47:18 -0500

    arreglando libro dto

--- src/libro/dto/create-libro.dto.ts
+++ src/libro/dto/create-libro.dto.ts
@@ -1,23 +1,36 @@
 // src/libro/dto/create-libro.dto.ts
-import { IsString, IsNotEmpty, IsOptional, IsDateString, IsBoolean, ValidateNested } from 'class-validator';
+import {
+    IsString,
+    IsNotEmpty,
+    IsOptional,
+    IsDateString,
+    IsBoolean,
+    ValidateNested,
+    IsNumber,
+    ValidateIf,
+} from 'class-validator';
 import { Type } from 'class-transformer';
 
 class CategoriaDto {
     @IsOptional()
+    @IsNumber()
     id?: number;
 
+    @ValidateIf(o => !o.id) // Solo valida nombre si no se pasó id
     @IsString()
     @IsNotEmpty()
-    nombre: string;
+    nombre?: string;
 }
 
 class AutorDto {
     @IsOptional()
+    @IsNumber()
     id?: number;
 
+    @ValidateIf(o => !o.id) // Solo valida nombre si no se pasó id
     @IsString()
     @IsNotEmpty()
-    nombre: string;
+    nombre?: string;
 }
 
 export class CreateLibroDto {
